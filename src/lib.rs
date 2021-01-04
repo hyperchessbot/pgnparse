4bcf0aa41df25f71c6dce65111e9ce9022d33157
hyperchessbot <hyperchessbot@gmail.com>
2021-01-04 16:00:20 +0000

    Include example in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! A library for parsing a PGN to Rust struct or JSON.
+//!
+//! # Examples
+//!
+//! Parse a variant PGN with custom starting position:
+//!
+//! ```
+//!use pgnparse::parser::*;
+//!
+//!fn main(){
+//!	let pgn = r#"[FEN "8/8/8/8/8/7k/8/7K w - - 0 1"]
+//![White "White"]
+//![Black "Black"]
+//![Variant "Atomic"]
+//!
+//!1. Kh2 Kg2
+//!"#;
+//!	
+//!	let result = parse_pgn_to_rust_struct(pgn.to_string());
+//!	
+//!	println!("{:?}", result);
+//!	
+//!	let result = parse_pgn_to_json_string(pgn.to_string());
+//!	
+//!	println!("{}", result);
+//!}
+//! ```
+//!
+
 pub mod parser;
 
 #[cfg(test)]
