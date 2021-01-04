8e35fd5cb1f1c9e7189fb76f572f7f8ccec372c7
hyperchessbot <hyperchessbot@gmail.com>
2021-01-04 08:59:55 +0000

    Parse headers

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,6 @@
 use shakmaty::variants::{Chess, Atomic, Antichess, KingOfTheHill, ThreeCheck, Crazyhouse, RacingKings, Horde};
+use pgn_reader::{Visitor, Skip, RawHeader, SanPlus, BufferedReader};
+use serde::{Deserialize, Serialize};
 
 /// lists possible variants together with position types representing them
 #[derive(Debug)]
@@ -31,5 +33,118 @@ pub fn position_from_variant_name(variant_name: &str) -> VariantPosition {
 		"rackingkings" | "racing kings" => VariantRacingKings { pos: RacingKings::default() },
 		"horde" => VariantHorde { pos: Horde::default() },
 		_ => VariantStandard { pos: Chess::default() },
+	}
+}
+
+/// san, uci, fen, epd for move
+#[derive(Debug, Serialize, Deserialize)]
+struct SanUciFenEpd {
+	san: String,
+	uci: String,
+	fen: String,
+	epd: String,
+}
+
+/// pgn headers and moves
+#[derive(Debug, Serialize, Deserialize)]
+struct PgnInfo {
+	headers: std::collections::HashMap<String, String>,
+	moves: Vec<SanUciFenEpd>,
+}
+
+/// implementation for PgnInfo
+impl PgnInfo {
+	fn new() -> PgnInfo {
+		PgnInfo {
+			headers: std::collections::HashMap::new(),
+			moves: vec!(),
+		}
+	}
+	
+	fn push(&mut self, san_uci_fen_epd: SanUciFenEpd) {
+		self.moves.push(san_uci_fen_epd);
+	}
+	
+	fn insert_header(&mut self, key: String, value: String) {
+		self.headers.insert(key, value);
+	}
+	
+	fn _get_header(&mut self, key:String) -> String {
+		self.headers.get(&key).unwrap_or(&"?".to_string()).to_string()
+	}
+}
+
+/// parsing state
+struct ParsingState{
+	pos: VariantPosition,
+	pgn_info: PgnInfo,
+}
+
+impl ParsingState{
+	fn new() -> ParsingState{
+		ParsingState{
+			pos: position_from_variant_name("standard"),
+			pgn_info: PgnInfo::new(),
+		}
+	}
+}
+
+/// implement visitor
+impl Visitor for ParsingState {
+    type Result = String;
+
+    fn header(&mut self, key: &[u8], value: RawHeader<'_>) {
+		let key_str_result = std::str::from_utf8(key);
+		match key_str_result {
+			Ok(key_str) => {
+				let value_str_result = std::str::from_utf8(value.as_bytes());
+				match value_str_result {
+					Ok(value_str) => {
+						self.pgn_info.insert_header(key_str.to_string(), value_str.to_string())
+					},
+					Err(err) => println!("header value utf8 parse error {:?}", err)
+				}
+			}
+			Err(err) => println!("header key utf8 parse error {:?}", err)
+		}		
+    }
+
+    fn begin_variation(&mut self) -> Skip {
+        Skip(true) // stay in the mainline
+    }
+
+    fn san(&mut self, san_plus: SanPlus) {
+		
+    }
+
+    fn end_game(&mut self) -> Self::Result {
+		let ser_result = serde_json::to_string(&self.pgn_info);
+		
+		match ser_result {
+			Ok(ser_str) => {
+				ser_str
+			},
+			Err(err) => {
+				println!("{:?}", err);
+				"".to_string()
+			}
+		}			
+    }
+}
+
+/// parse pgn to json string
+pub fn parse_pgn_to_json_string(pgn_str: String) -> String {
+	let pgn_bytes = pgn_str.as_bytes();
+		
+	let mut reader = BufferedReader::new_cursor(&pgn_bytes);
+
+	let mut visitor = ParsingState::new();
+	
+	match reader.read_game(&mut visitor) {
+		Ok(moves_opt) => moves_opt.unwrap_or("".to_string()),
+		Err(err) => {
+			println!("{:?}", err);
+			"".to_string()
+		}
 	}
 }
--- src/pgnparse.rs
+++ src/pgnparse.rs
@@ -1,7 +1,14 @@
 use pgnparse::parser::*;
 
 fn main(){
-	let pos = position_from_variant_name("horde");
+	let pgn = r#"[Variant "Atomic"]
+[White "White"]
+[Black "Black"]
+
+1. Nf3 f6 2. e3 e6
+"#;
 	
-	println!("{:?}", pos);
+	let result = parse_pgn_to_json_string(pgn.to_string());
+	
+	println!("{}", result);
 }
